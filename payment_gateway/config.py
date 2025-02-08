a2d715741d1b56a96e2f51ae839aef7e884d4a1f
israel <20001637@devops.insa.gov.et>
2025-02-08 15:08:59 +0300

    add some parameters on config

--- payment_gateway/config.py
+++ payment_gateway/config.py
@@ -1,2 +1,8 @@
-API_BASE_URL = "https://api.paymentgateway.com/v1"
-TIMEOUT = 30  # Request timeout in seconds
+class Config:
+    CONSUMER_KEY = "your_consumer_key"
+    CONSUMER_SECRET = "your_consumer_secret"
+    SHORTCODE = "your_shortcode"
+    PASSKEY = "your_passkey"
+    CALLBACK_URL = "https://your_callback_url.com"
+    BASE_URL = "https://sandbox.safaricom.co.ke"
+    TIMEOUT = 30
